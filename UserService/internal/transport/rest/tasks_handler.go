4e2204cead3349c215b00b9726afd1333ef6c993
Gollum Malka <gmalka@un-r2.kzn.21-school.ru>
2023-06-27 10:13:39 +0300

    4321

--- UserService/internal/transport/rest/tasks_handler.go
+++ UserService/internal/transport/rest/tasks_handler.go
@@ -51,39 +51,6 @@ func (h Handler) getUsersTasksWithoutAnswer(w http.ResponseWriter, r *http.Reque
 	w.Write(b)
 }
 
-func (h Handler) getTasksWithoutAnswer(w http.ResponseWriter, r *http.Request) {
-	page := chi.URLParam(r, "page")
-
-	pageNum, err := strconv.Atoi(page)
-	if err != nil {
-		h.logger.Err.Printf("can't parse number %s: %v", page, err.Error())
-		http.Error(w, "message: some server error", http.StatusInternalServerError)
-		return
-	}
-
-	if pageNum == 0 {
-		pageNum = 1
-	}
-
-	orders, err := h.grpcCli.GetAllTasksWithoutAnswers(pageNum)
-	if err != nil {
-		h.logger.Err.Println(err.Error())
-		http.Error(w, fmt.Sprintf("message: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-
-	b, err := json.Marshal(orders)
-	if err != nil {
-		h.logger.Err.Printf("marshal error: %v\n", err.Error())
-		http.Error(w, "message: some server error", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("page №%d:\n", pageNum)))
-	w.Write(b)
-}
-
 func (h Handler) getAllTasks(w http.ResponseWriter, r *http.Request) {
 	u, ok := r.Context().Value(UserRequest{}).(tokenManager.UserClaims)
 	if !ok {
